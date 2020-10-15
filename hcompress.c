56df67939c1c3617c14591980e711b668a6407ce
Gavin Hailey <gavin.hailey@du.edu>
2020-10-15 15:44:16 -0600

    initial commit

---
+++ hcompress.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <strings.h>
+#include "linkedList.h"
+
---
+++ linkedList.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <strings.h>
+#include "linkedList.h"
